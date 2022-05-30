0c34ee27c02b4a0e7371570f92224d540960c5fb
liviusgrosu <livius.grosu@ryerson.ca>
2022-05-29 20:23:05 -0400

    Generate and toggle visibility of chunks depending on viewers position in world space

---
+++ Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+    public const float maxViewDistance = 450;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    private int chunkSize;
+    private int chunkVisibileInViewDistance;
+
+    private Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+
+    private void Start()
+    {
+        // Actual size of the mesh is 1 less then the inputted chunk size
+        chunkSize = MapGenerator.maxChunkSize - 1;
+        // 300 / 240 = 1
+        chunkVisibileInViewDistance = Mathf.RoundToInt(maxViewDistance / chunkSize);
+    }
+
+    private void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    private void UpdateVisibleChunks()
+    {
+        for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++)
+        {
+            terrainChunksVisibleLastUpdate[i].SetVisible(false);
+        }
+        terrainChunksVisibleLastUpdate.Clear();
+
+        // Get the chunk indices that the player is on
+        int currentChunkCoordinateX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordinateY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for (int yOffset = -chunkVisibileInViewDistance; yOffset < chunkVisibileInViewDistance; yOffset++)
+        {
+            for (int xOffset = -chunkVisibileInViewDistance; xOffset < chunkVisibileInViewDistance; xOffset++)
+            {
+                Vector2 viewedChunkCoordinate = new Vector2(currentChunkCoordinateX + xOffset, currentChunkCoordinateY + yOffset);
+
+                if (terrainChunkDictionary.ContainsKey(viewedChunkCoordinate))
+                {
+                    terrainChunkDictionary[viewedChunkCoordinate].UpdateTerrainChunk();
+                    if (terrainChunkDictionary[viewedChunkCoordinate].IsVisible())
+                    {
+                        terrainChunksVisibleLastUpdate.Add(terrainChunkDictionary[viewedChunkCoordinate]);
+                    }
+                }
+                else
+                {
+                    terrainChunkDictionary.Add(viewedChunkCoordinate, new TerrainChunk(viewedChunkCoordinate, chunkSize, transform));
+                }
+            }
+        }
+    }
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 coordinate, int size, Transform parent)
+        {
+            position = coordinate * size;
+            // Create a bounds to get the distance to edge
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = positionV3;
+            // Default primitive plane is size 10 so we scale it back by 10
+            meshObject.transform.localScale = Vector3.one * size / 10f;
+            meshObject.transform.parent = parent;
+            SetVisible(false);
+        }
+
+        public void UpdateTerrainChunk()
+        {
+            // Using a bounds, we get the closest distance from bound box and viewer position
+            float viewerDistanceToNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            // Determine if we can draw the chunk
+            bool visible = viewerDistanceToNearestEdge <= maxViewDistance;
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool IsVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
